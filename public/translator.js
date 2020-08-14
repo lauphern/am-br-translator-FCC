fcc54560269f02351d6c0fe18cf2226b7079ea0d
lauphern <lau.p.hern@hotmail.com>
2020-08-14 12:49:04 +0200

    added methods to create paragraphs and spans

--- public/translator.js
+++ public/translator.js
@@ -3,13 +3,39 @@ import { britishOnly } from './british-only.js';
 import { americanToBritishSpelling } from './american-to-british-spelling.js';
 import { americanToBritishTitles } from './american-to-british-titles.js';
 
+export class Translator {
+  constructor() {
+    this.textArea = document.querySelector("#text-input");
+    this.translatedSentenceDiv = document.querySelector("#translated-sentence");
+    this.errorMsgDiv = document.querySelector("#error-msg");
+    this.localeSelect = document.querySelector("#locale-select");
+    this.translateBtn = document.querySelector("#translate-btn");
+    this.clearBtn = document.querySelector("#clear-btn");
+  }
+
+  createParagraph = str => {
+    let p = document.createElement("p");
+    p.innerText = str;
+    return p;
+  }
+
+  createSpan = str => {
+    let span = document.createElement("span");
+    span.innerText = str;
+    return span;
+  }
+}
+
+const translator = new Translator();
+
+
 /* 
   Export your functions for testing in Node.
   Note: The `try` block is to prevent errors on
   the client side
 */
-try {
-  module.exports = {
+// try {
+//   module.exports = {
 
-  }
-} catch (e) {}
+//   }
+// } catch (e) {}
